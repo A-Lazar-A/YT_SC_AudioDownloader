0256e4e163582cb880366e7eccb8548fac4d9ca5
A-Lazar-A <fub.lazar@mail.ru>
2022-05-10 16:25:29 +0300

    SoundCloud module added

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import click
 from art import text2art
 from pytube import YouTube
+import subprocess
+import json
+import requests
+
 
 @click.command()
 @click.argument('url')
-@click.option('--path', default='', help='Path to the download folder')
+@click.option('--path', default='.', help='Path to the download folder')
 def downloader(url, path):
     print(text2art('YT SC Downloader'))
     if 'youtube.com' in url:
@@ -13,13 +18,27 @@ def downloader(url, path):
             streams = file.streams
             audio_best = streams.get_audio_only()
             audio_best.download(output_path=path)
+
+        except Exception as e:
+            print(e)
+    elif 'soundcloud.com' in url:
+        try:
+            subprocess.call(['youtube-dl', '--write-info-json', '-o', f'tmp/%(title)s.%(ext)s', '--skip-download', url])
+            json_file = [pos_json for pos_json in os.listdir('tmp') if pos_json.endswith('.json')]
+
+            with open(f'tmp/{json_file[0]}', 'r') as fl:
+                data = json.load(fl)
+            download_url = data['url']
+            full_title = data['fulltitle']
+
+            with open(f'{path}/{full_title}.mp3', 'wb') as fl:
+                fl.write(requests.get(download_url, stream=True).content)
+
         except Exception as e:
             print(e)
     else:
         print('Invalid URL')
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     downloader()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
